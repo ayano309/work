0d8d28e6e167212cb2e5108faa040e750c1c013c
ayano309 <ayatomato.k@gmail.com>
2022-06-04 09:37:36 +0000

    devise-user-Routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,21 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
+  
+  # user関連
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords',
+    :confirmations => 'users/confirmations',
+    :unlocks => 'users/unlocks',
+  }
+
+  # userのサインアップなど
+  devise_scope :user do
+    get 'signup', :to => 'users/registrations#new'
+    get 'login', :to => 'users/sessions#new'
+    delete 'logout', :to => 'users/sessions#destroy'
+  end
+
 end
