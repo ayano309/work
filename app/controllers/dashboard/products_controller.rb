847463e3cec3ce4bb86abfe258dbfe21fce1c946
ayano309 <ayatomato.k@gmail.com>
2022-06-04 11:18:50 +0000

    [fix]admin-product-controller

--- app/controllers/dashboard/products_controller.rb
+++ app/controllers/dashboard/products_controller.rb
@@ -1,2 +1,48 @@
 class Dashboard::ProductsController < ApplicationController
+  before_action :authenticate_admin!
+  before_action :set_product, only: %w[show edit update destroy]
+  layout 'dashboard/dashboard'
+
+  def index
+    @product = Product.all
+  end
+
+  def new
+    @product = Product.new
+    @categories = Category.all
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to dashboard_products_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @categories = Category.all
+  end
+
+  def update
+    @product.update(product_params)
+    redirect_to dashboard_products_path
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to dashboard_products_path
+  end
+
+  
+
+  private
+    def set_product
+      @product = Product.find(params[:id])
+    end
+    
+    def product_params
+      params.require(:product).permit(:name, :description, :price, :soldout_flag, :category_id, :image)
+    end
 end
