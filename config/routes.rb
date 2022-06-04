3303c83f6535ec6171bf460c6c64d1f0a511ef22
ayano309 <ayatomato.k@gmail.com>
2022-06-04 10:08:11 +0000

    add-devise-admin-Routing

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,18 @@ Rails.application.routes.draw do
     get 'login', :to => 'users/sessions#new'
     delete 'logout', :to => 'users/sessions#destroy'
   end
+  
+  devise_for :admins, :controllers => {
+     :sessions => 'admins/sessions'
+   }
+ 
+   devise_scope :admin do
+     get "dashboard", :to => "dashboard#index"
+     get "dashboard/login", :to => "admins/sessions#new"
+     post "dashboard/login", :to => "admins/sessions#create"
+     delete "dashboard/logout", :to => "admins/sessions#destroy"
+   end
 
-   get "dashboard", :to => "dashboard#index"
+
+
 end
