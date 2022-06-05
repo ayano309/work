66109c7b823194da60eeac49771a9c6133e2daf9
ayano309 <ayatomato.k@gmail.com>
2022-06-05 10:12:04 +0000

    税込、小計、合計表示

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,3 +2,16 @@ class CartItem < ApplicationRecord
   belongs_to :product
   belongs_to :user
 
+
+## 小計を求めるメソッド
+def subtotal
+    product.with_tax_price * amount
+end
+
+def total
+  subtotal += subtotal
+end
+
+
+
+end
