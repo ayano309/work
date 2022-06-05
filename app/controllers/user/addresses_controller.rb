05955056d85c9acec4f49f2f0ed93cc595fed1e6
ayano309 <ayatomato.k@gmail.com>
2022-06-05 10:21:02 +0000

    [add]create-address-controller

---
+++ app/controllers/user/addresses_controller.rb
@@ -0,0 +1,30 @@
+class User::AddressesController < ApplicationController
+  before_action :authenticate_user!
+  
+ def index
+   
+ end
+ 
+ def create
+   
+ end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+    
+  end
+  
+  def destroy
+    
+  end
+  private
+  
+  def address_params
+    
+  end
+  
+end
