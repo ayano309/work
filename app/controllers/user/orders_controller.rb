036fafda008dde079428b2b1f08308c91fd45d9a
ayano309 <ayatomato.k@gmail.com>
2022-06-06 05:58:07 +0000

    注文してありがとう表示まで

--- app/controllers/user/orders_controller.rb
+++ app/controllers/user/orders_controller.rb
@@ -1,46 +1,88 @@
 class User::OrdersController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
-    
+
   end
-  
+
   def show
-    
+
   end
-  
+
   def new
     @order = current_user.orders.new
     @addresses = current_user.addresses
   end
-  
+
   def confirm
-    @order = Order.new(order_params)
-    binding.pry #追記する
+    @order = Order.new(
+      user: current_user,
+      pay_method: params[:order][:pay_method]
+    )
+
+    # カートアイテムの情報
+    @cart_items = current_user.cart_items
+
+    if params[:order][:user_address] = 1
+      @order.zip_code = current_user.zip_code
+      @order.address = current_user.address
+      @order.name = current_user.last_name + current_user.first_name
+    elsif params[:order][:user_address] = 2
+      @address = current_user.addresses.find(params[:Address][:select_id])
+      @order.zip_code = @address.zip_code
+      @order.address = @address.address
+      @order.name = @address.name
+    elsif
+      current_user.addresses.create!(
+        zip_code: @order.zip_code,
+        address: @order.address,
+        name: @order.name
+        )
+    end
   end
 
-  
+
   def create
-  
+    @order = current_user.orders.new(order_params)
+    @order.save
+
+    #注文履歴に移動
+    @cart_items = current_user.cart_items
+    @cart_items.each do |cart_item|
+      @order_detail = OrderDetail.new
+      @order_detail.product_id = cart_item.product_id
+      @order_detail.order_id = @order.id
+      @order_detail.amount = cart_item.amount
+      @order_detail.tax_included_price = cart_item.product.with_tax_price * cart_item.amount
+      @order_detail.save
+    end
+    # 最後にカート商品を全て削除する
+    @cart_items.destroy_all
+    redirect_to user_thanks_path
+
   end
-  
-  def edit 
-    
+
+  #ありがとうのページ
+  def thanks
   end
-  
+
+  def edit
+
+  end
+
   def update
-    
+
   end
-  
-  
+
+
   def destroy
-    
+
   end
-  
+
   private
-  
+ 
   def order_params
-    params.require(:order).(:pay_method,:zip_code,:address,:name)
+    params.require(:order).permit(:pay_method,:zip_code,:address,:name,:total_price)
   end
-  
+
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,15 +3,4 @@ class CartItem < ApplicationRecord
   belongs_to :user
 
 
-## 小計を求めるメソッド
-def subtotal
-    product.with_tax_price * amount
-end
-
-def total
-  subtotal += subtotal
-end
-
-
-
 end
