1fa487e1dde83dd9c66052758e95e6e8171ce2ab
ayano309 <ayatomato.k@gmail.com>
2022-06-06 07:09:28 +0000

    管理者側のorder-controllerのアクション追加

--- app/controllers/dashboard/orders_controller.rb
+++ app/controllers/dashboard/orders_controller.rb
@@ -1,2 +1,27 @@
 class Dashboard::OrdersController < ApplicationController
+  before_action :authenticate_admin!
+  layout 'dashboard/dashboard'
+  
+  def index
+    @order = Order.all
+  end
+  
+  def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+  end
+  
+  def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    flash[:notice] = "注文ステータスの変更しました"
+    redirect_to dashboard_orders_path
+  end
+  
+  
+  
+  private 
+  def order_params
+    params.require(:order).permit(:order_status)
+  end
 end
